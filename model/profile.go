236e65119a6ddb61413df29290f7bba6481f9768
the-stardust <wangyuhao2580@gmail.com>
2019-12-07 17:20:30 +0800

    README

---
+++ model/profile.go
@@ -0,0 +1,14 @@
+package model
+
+type Profile struct {
+	Name string
+	Age string
+	Sex string
+	Height string
+	Weight string
+	Income string
+	Marriage string
+	NativePlace string
+	WorkPlace string
+	Child string
+}
